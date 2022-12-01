283d8a578b20084dcda25582c9eb55bf5e47ad48
lidankatzav <lidankatzav5@gmail.com>
2022-12-01 13:21:08 +0200

    main

--- src/dist/logic.js
+++ src/dist/logic.js
@@ -1,23 +1,47 @@
 var list = [];
+var counter = 0;
+var status_lst = [];
 function get_word() {
     var user_input = document.getElementById("new-todo");
     list.push(user_input.value);
-    console.log(list);
     user_input.value = "";
 }
 function add_to_list() {
-    var _a, _b, _c;
+    var _a, _b, _c, _d;
     var new_temp = document.getElementById("template");
     var childs = (_b = (_a = new_temp.content.firstElementChild) === null || _a === void 0 ? void 0 : _a.firstElementChild) === null || _b === void 0 ? void 0 : _b.children;
     childs[1].innerHTML = list[list.length - 1];
     (_c = document
         .getElementById("todo-list")) === null || _c === void 0 ? void 0 : _c.append(new_temp.content.cloneNode(true));
+    counter += 1;
+    update_counter();
+    var children = (_d = document.getElementById("todo-list")) === null || _d === void 0 ? void 0 : _d.children;
+    // children[children.length - 1].firstElementChild?.children[2].addEventListener(
+    // "click",
+    // () => {
+    // }
+    // );
+}
+function clear_list() {
+    var e = document.getElementById("todo-list");
+    e.innerHTML = "";
+    list = [];
+    counter = 0;
+    update_counter();
+}
+function update_counter() {
+    document.getElementById("counter").innerHTML =
+        String(counter) + " items left";
 }
 document.addEventListener("DOMContentLoaded", function () {
+    var _a;
     document.getElementById("new-todo").addEventListener("keyup", function (e) {
         if (e.key === "Enter") {
             get_word();
             add_to_list();
         }
+    });
+    (_a = document.getElementById("clear")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+        clear_list();
     });
 });
--- src/logic.ts
+++ src/logic.ts
@@ -1,9 +1,10 @@
 let list: string[] = [];
+let counter: number = 0;
+let status_lst: boolean[] = [];
 
 function get_word(): void {
   const user_input = document.getElementById("new-todo") as HTMLInputElement;
   list.push(user_input.value);
-  console.log(list);
   user_input.value = "";
 }
 
@@ -15,6 +16,29 @@ function add_to_list(): void {
   document
     .getElementById("todo-list")
     ?.append(new_temp.content.cloneNode(true));
+  counter += 1;
+  update_counter();
+  const children = document.getElementById("todo-list")
+    ?.children as HTMLCollection;
+  // children[children.length - 1].firstElementChild?.children[2].addEventListener(
+  // "click",
+  // () => {
+
+  // }
+  // );
+}
+
+function clear_list() {
+  const e = document.getElementById("todo-list") as HTMLElement;
+  e.innerHTML = "";
+  list = [];
+  counter = 0;
+  update_counter();
+}
+
+function update_counter() {
+  (document.getElementById("counter") as HTMLElement).innerHTML =
+    String(counter) + " items left";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -27,4 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   );
+  document.getElementById("clear")?.addEventListener("click", () => {
+    clear_list();
+  });
 });
